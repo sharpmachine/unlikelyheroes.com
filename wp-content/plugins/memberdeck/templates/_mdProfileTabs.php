a43e82895089334dad1c373116c8241074531ab4
Jesse Kade <jessekade@gmail.com>
2014-01-07 15:47:25 -0800

    fixing campaign process

--- wp-content/plugins/memberdeck/templates/_mdProfileTabs.php
+++ wp-content/plugins/memberdeck/templates/_mdProfileTabs.php
@@ -18,5 +18,6 @@ if (!empty($dash)) {
 	<ul class="nav nav-tabs">
 	<li><a href="<?php echo $durl; ?>"><?php _e('My Dashboard', 'memberdeck'); ?></a></li>
 	<li><a href="<?php echo (isset($current_user) ? the_permalink().'?edit-profile='.$current_user->ID : ''); ?>"><?php echo (isset($current_user) ? __('My Profile', 'memberdeck') : ''); ?></a></li>
+	<li></li>
 	<?php do_action('md_profile_extratabs'); ?>
 </ul>
