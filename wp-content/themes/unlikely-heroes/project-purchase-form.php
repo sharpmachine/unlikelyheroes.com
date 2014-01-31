f4f99ae9915496309f31b303b490e85c95f2ece2
Jesse Kade <jessekade@gmail.com>
2014-01-30 19:16:27 -0800

    tweaks form CSS overrides

--- wp-content/themes/unlikely-heroes/project-purchase-form.php
+++ wp-content/themes/unlikely-heroes/project-purchase-form.php
@@ -4,12 +4,25 @@ $id = $post->ID;
 $content = the_project_content($id);
 $project_id = get_post_meta($id, 'ign_project_id', true);
 ?>
+
+<div class="jumbotron">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>
+					<span>Support Campaign</span>
+				</h1>
+			</div>
+		</div>
+	</div>
+</div>
+
 <div class="container">
 	<div class="row">
-		<div class="col-xs-12">
+		<div class="col-xs-12 col-md-10 col-md-offset-1">
 			<div id="site-description">
 				<h1 class="text-center"><?php echo $content->name; ?></h1>
-				<h2 class="text-center"><?php echo $content->short_description; ?></h2> 
+				<h3 class="text-center"><?php echo $content->short_description; ?></h3> 
 			</div>
 			<div class="entry-content">
 				<?php
